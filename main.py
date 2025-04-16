a4d1509fb0d0f3cbe469cb4674cbf3a3d14b466e
waleed2112 <l.hwk@hotmail.com>
2025-04-17 02:33:07 +0300

    تحديث الكود لدعم ترجمة القصة

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def handle_message(update, context):
 
     if response["Response"] == "True":
         translated_plot = translator.translate(response["Plot"], dest='ar').text
+
         reply = f"""
 *العنوان:* {response['Title']}
 *السنة:* {response['Year']}
@@ -28,7 +29,13 @@ def handle_message(update, context):
 *النوع:* {response['Genre']}
 *القصة:* {translated_plot}
 """
-        update.message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
+
+        poster_url = response.get("Poster", "")
+
+        if poster_url and poster_url != "N/A":
+            update.message.reply_photo(photo=poster_url, caption=reply, parse_mode=ParseMode.MARKDOWN)
+        else:
+            update.message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
     else:
         update.message.reply_text("لم أتمكن من العثور على هذا العنوان، تأكد من كتابة الاسم بشكل صحيح.")
 
